f3aeab8a338901bca754d783df1d97469e7e41ef
Bilosca <Biloscagabriel@hotmail.com>
2020-12-09 22:26:20 -0300

    Separação das classes e metodos da função principal.

---
+++ controle_main.py
@@ -0,0 +1,45 @@
+from controle_metodos import PerdasDB, insereDados
+
+#Arquivo principal cujo o programa importa as classes e metodos e roda
+
+perdas = PerdasDB("controle_perdas.db")
+letras = ['i', 'e', 'b', 'd', 'f']
+perdas.atualizaDias_e_Notifica()
+while True:
+    print("\nPressione a Letra pra escolher sua opcao: ")
+    print()
+
+    try:
+        decision = input("(I) - Inserir Remessa \n(E) - Editar Remessa \n(B) - Buscar Remessa\
+            \n(D) - Deletar Remessa \n(F) - Fechar Programa \n\n")
+        
+        decision = decision.lower()
+
+        if decision not in letras:
+            print("(Opcao Nao disponivel !)\n")
+            continue
+
+        if decision == 'i':
+            print("\nRegras: \n1° - Nome \n2° - Marca")
+            produto, setor, dia, mes, ano = insereDados(0)
+            perdas.insereRemessa(produto, setor, dia, mes, ano)
+        
+        elif decision =='e':
+            ident, produto, setor = insereDados(1)
+            perdas.editarRemessa(ident, produto, setor)
+        
+        elif decision == 'b':
+            produto = insereDados(2)
+            perdas.buscarProduto(produto)
+
+        elif decision == 'd':
+            ident = input("\nId da Remessa: ")
+            perdas.excluiRemessa(ident)
+                    
+        elif decision == 'f':
+            perdas.fechar()
+            break
+
+    except Exception as erro:
+        print(erro)
+        pass 
--- controle_metodos.py
+++ controle_metodos.py
@@ -1,6 +1,8 @@
 import sqlite3
 import datetime
 
+#Arquivo que contem os metodos e classes
+
 class PerdasDB:
     def __init__(self, arquivoDB):
         self.conex = sqlite3.connect(arquivoDB)
@@ -162,47 +164,4 @@ def insereDados(opcao = 0):
     elif opcao == 2:
         return produto
 
-
-if __name__ == "__main__":
-    perdas = PerdasDB("BancoDeDados.db")
-    letras = ['i', 'e', 'b', 'd', 'f']
-    perdas.atualizaDias_e_Notifica()
-    while True:
-        print("\nPressione a Letra pra escolher sua opcao: ")
-        print()
-
-        try:
-            decision = input("(I) - Inserir Remessa \n(E) - Editar Remessa \n(B) - Buscar Remessa\
-                \n(D) - Deletar Remessa \n(F) - Fechar Programa \n\n")
-            
-            decision = decision.lower()
-
-            if decision not in letras:
-                print("(Opcao Nao disponivel !)\n")
-                continue
-
-            if decision == 'i':
-                print("\nRegras: \n1° - Nome \n2° - Marca")
-                produto, setor, dia, mes, ano = insereDados(0)
-                perdas.insereRemessa(produto, setor, dia, mes, ano)
-            
-            elif decision =='e':
-                ident, produto, setor = insereDados(1)
-                perdas.editarRemessa(ident, produto, setor)
-            
-            elif decision == 'b':
-                produto = insereDados(2)
-                perdas.buscarProduto(produto)
-
-            elif decision == 'd':
-                ident = input("\nId da Remessa: ")
-                perdas.excluiRemessa(ident)
-                        
-            elif decision == 'f':
-                perdas.fechar()
-                break
-
-        except Exception as erro:
-            print(erro)
-            pass 
 
